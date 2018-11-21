26606a2bb49c7a668e8f5d561edc54c7047aea2d
Andre Briggs <andrebriggs@users.noreply.github.com>
2018-11-20 19:26:49 -0800

    Changes to order and primary keys

--- db/tables/010_training_info.sql
+++ db/tables/010_training_info.sql
@@ -4,6 +4,7 @@ CREATE TABLE Training_Info (
     ModelLocation text NOT NULL,
     --Consider additional metadata like a path to zip file
     --containing the pipeline.config, model, etc.
+    CreatedByUser integer REFERENCES User_Info(UserId),
     ModifiedDtim timestamp NOT NULL default current_timestamp,
     CreatedDtim timestamp NOT NULL default current_timestamp
 );
